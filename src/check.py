a548f2773e2c62010163a4eb698cb9b7af2aa3d1
susyexists <susy@selectron.me>
2024-03-24 16:30:39 -0400

    parameter check added

---
+++ src/check.py
@@ -0,0 +1,12 @@
+def project_id(project_id):
+    if (project_id==False):
+        raise Exception("Define a project_id")
+    
+def job_id(job_id):
+    if (job_id==False):
+        raise Exception("Define a job_id")
+    
+def config(config):
+    if config==False:
+        raise Exception("Define a configuration")
+
