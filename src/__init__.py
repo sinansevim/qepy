d24adc7b3179dca2980aefe8cf68b5b48a789b98
susyexists <susy@selectron.me>
2024-05-15 22:49:28 -0400

    pip package publish

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .project import project
-from .ph import ph
-from .utils import minimum_energy
---
+++ src/espresso_machine/__init__.py
@@ -0,0 +1,4 @@
+from .project import project
+from .ph import ph
+from .utils import minimum_energy
+from . import reads
